f190bf4adb6c272b267303d751a285e8e6498bb3
edmendes <eduardo16lmendes@gmail.com>
2018-04-01 14:47:39 -0300

    Correcting and testing

--- Morse/BuildFiles/builder.py
+++ Morse/BuildFiles/builder.py
@@ -32,7 +32,7 @@ motion.add_stream('ros')
 
 #env = Environment('indoors-1/indoor-1')
 
-env = Environment('/home/eduardo/Documents/TG_OFFICIAL/Morse1/estacionamento')
+env = Environment('/home/eduardo/Documents/TG_OFFICIAL/Morse/estacionamento')
 env.set_camera_location([5, -5, 6])
 env.set_camera_rotation([1.0470, 0, 0.7854])
 
