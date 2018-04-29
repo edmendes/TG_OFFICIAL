5f2fa03212ac1a769872f8241452e66d6e5d4b37
nathliapaula <nathalia.paula@ymail.com>
2018-04-29 11:09:25 -0300

    Identificação Obj Passivo com LaserScan

--- morse_tut/morse_tut_scene.py
+++ morse_tut/morse_tut_scene.py
@@ -1,35 +1,41 @@
 from morse.builder import *
- 
+
 atrv = ATRV() #the robot
-atrv.translate(x=2.5, y=3.2, z=0.0) #starting location
- 
-# An odometry sensor to get odometry information
-odometry = Odometry()
-atrv.append(odometry)
-odometry.add_interface('ros', topic="/odom",frame_id="odom", child_frame_id="base_link")
+atrv.translate(x=-31, z=-4.9) #starting location
+atrv.rotate(z=3.14/2)
+
+#adding a passive object
+table = PassiveObject('props/objects','SmallTable')
+table.setgraspable()
+table.translate(x=-31, y=5, z=-4.9)
+table.properties(Type = "table", Object = True, Graspable = True, Label = "TABLE")
  
 # Allows to control the robot with the keyboard
 keyboard = Keyboard()
 atrv.append(keyboard)
- 
-#camera 10Hz, 320x240 image
-cam_frequency = 10
-camera = VideoCamera()
-camera.translate(x = 0.7, z= 0.5)
-camera.rotate(y = -0.0)
-camera.properties(cam_width=320,cam_height=240,cam_focal=6.,cam_near=0.1,cam_far=500)
-camera.frequency(cam_frequency)
-atrv.append(camera)
-camera.add_interface('ros',topic='/camera')
+
+
+"""
+SENSORS: odometria, laserscan
+"""
+# An odometry sensor to get odometry information
+odometry = Odometry()
+atrv.append(odometry)
+odometry.add_interface('ros', topic="/odom",frame_id="odom", child_frame_id="base_link")
+
+pose = Pose()
+pose.translate(z=0.83)
+atrv.append(pose)
+pose.add_interface('ros', topic='/pose')
  
 #laser 10Hz, 180 degrees wide with a 180 points
-scan = Hokuyo()
+scan = Sick()
 scan.translate(x=0.275, z=0.252)
-atrv.append(scan)
-scan.properties(Visible_arc = True,laser_range = 30.0,resolution = 1,scan_window = 180.0)
-scan.frequency(10)
+scan.properties(Visible_arc = True,laser_range = 10.0,resolution = 1,scan_window = 180.0)
+scan.frequency(5)
 scan.create_laser_arc()
 scan.add_interface('ros', topic='/scan',frame_id="laser", child_frame_id="base_link")
+atrv.append(scan)
  
 #motion controller
 motion = MotionXYW()
@@ -37,4 +43,6 @@ atrv.append(motion)
 motion.add_interface('ros', topic='/cmd_vel')
  
 # Set the environment
-env = Environment('indoors-1/indoor-1')
+env = Environment('/home/nana/morse-1.3/data/environments/estacionamento/estacionamento')
+env.set_camera_location([-25, -7, 2])
+env.set_camera_rotation([1.0470, 0, 0.7854])
